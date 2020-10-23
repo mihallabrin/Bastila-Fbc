87a83c692007c9b0939b7c47f4cd0c5ebe1eab1e
mickersicc <mickersicc@gmail.com>
2020-10-23 19:18:20 -0400

    added auth

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -1,5 +1,10 @@
 const devConfig = {
-  "aws_project_region": "us-east-1"
-}
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": "us-east-1:21172e4c-9b74-498e-9ba2-b101da3c0f78",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_yCWFeV2Iz",
+  "aws_user_pools_web_client_id": "7mhv055rkg3g95svusinv42j24",
+  "oauth": {}
+};
 
 export default devConfig;
