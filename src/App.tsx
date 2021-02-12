135ee3e80b940de8cbc436a2213720c2b78c511b
mickersicc <mickersicc@gmail.com>
2021-02-12 12:40:56 -0500

    missed changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import React, { useEffect, useState } from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { listBlogs } from './graphql/queries';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+
+import Dynamsoft from 'dynamsoft-javascript-barcode';
 
 import './App.css';
 
 function App() {
+  const [blogs, setBlogs] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  async function fetchBlogs() {
+    try {
+      const result: any = await API.graphql(graphqlOperation(listBlogs));
+      const blogs = result.data.listBlogs.items;
+
+      setBlogs(blogs);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let scanner = null;
+
+  (async () => {
+    scanner = await Dynamsoft.BarcodeScanner.createInstance();
+    scanner.onFrameRead = (results) => {
+      console.log(results);
+    };
+    scanner.onUnduplicatedRead = (txt, result) => {
+      alert(txt);
+    };
+    await scanner.show();
+  })();
+
   return (
     <div className='App'>
       <header className='App-header'>Header</header>
+      <div>
+        {blogs.map((blog: any) => (
+          <div key={blog.id}>{blog.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -4,7 +4,10 @@ const devConfig = {
   "aws_cognito_region": "us-east-1",
   "aws_user_pools_id": "us-east-1_yCWFeV2Iz",
   "aws_user_pools_web_client_id": "7mhv055rkg3g95svusinv42j24",
-  "oauth": {}
+  "oauth": {},
+  "aws_appsync_graphqlEndpoint": "https://jjf545yvlvcfzfwssgxjtbrqvu.appsync-api.us-east-1.amazonaws.com/graphql",
+  "aws_appsync_region": "us-east-1",
+  "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 export default devConfig;
